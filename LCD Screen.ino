aa9e9c41a3c7b08e27b0f1d672d21baf46c225a7
Simon Tallmyr <simon@tallmyr.se>
2021-07-03 00:15:27 +0100

    Fixed typos, slight refactor, added comments

--- LCD Screen.ino
+++ LCD Screen.ino
@@ -31,30 +31,32 @@ void setup()
   pinMode(D6, OUTPUT);
   pinMode(D7, OUTPUT);
 
-  //Define Output
+  // Define Output
   char output[] = "Simon kan koda!";
 
-  //Init
-  send_command(false, false, 0b00000001);   //Clear Display
-  send_command(false, false, 0b0000000010); //Return Home
-  send_command(false, false, 0b0000001111); //Dispay on, cursor on, blinking on
+  // Init display
+  send_command(false, false, 0b00000001); //Clear Display
+  send_command(false, false, 0b00000010); //Return Home
+  send_command(false, false, 0b00001111); //Dispay on, cursor on, blinking on
+
+  // Send text
   generate_text(output);
-  send_command(false, false, 0b0000001100); //Display on, cursor off, blinking off
+
+  // Stop blinking and stuff
+  send_command(false, false, 0b00001100); //Display on, cursor off, blinking off
 }
 
 void generate_text(char *ascii_str)
 {
   int len = strlen(ascii_str);
-  int arr[len];
+  int ascii_number[len];
   int i;
-  int bytes;
 
-  string2bytes(ascii_str, arr);
+  string2bytes(ascii_str, ascii_number);
 
   for (i = 0; i < len; i++)
   {
-    bytes = arr[i];
-    send_command(true, false, bytes);
+    send_command(true, false, ascii_number[i]);
   }
 }
 
@@ -72,6 +74,11 @@ void string2bytes(char *input, int *output)
   }
 }
 
+/*
+<bool> rs - Register Select. False for instructions, True for Data.
+<bool> rw - Read/Write. Read not implemented here, always set false in order to write.
+<int> data - 8 bit data for instruction when RS is false, or 8 bit ascii character when RS is set true.
+*/
 void send_command(bool rs, bool rw, int data)
 {
 
@@ -85,7 +92,7 @@ void send_command(bool rs, bool rw, int data)
     digitalWrite(RW, HIGH);
   }
 
-  //Data Signals
+  // Set Data signals
   digitalWrite(D7, HIGH && (data & 0b10000000));
   digitalWrite(D6, HIGH && (data & 0b01000000));
   digitalWrite(D5, HIGH && (data & 0b00100000));
@@ -95,14 +102,12 @@ void send_command(bool rs, bool rw, int data)
   digitalWrite(D1, HIGH && (data & 0b00000010));
   digitalWrite(D0, HIGH && (data & 0b00000001));
 
-  //Write Enable
+  // Toggle Enable
   digitalWrite(E, HIGH);
-
-  //Delay
   delay(1);
-
-  //Disable all signals
   digitalWrite(E, LOW);
+
+  // Disable all signals
   digitalWrite(D0, LOW);
   digitalWrite(D1, LOW);
   digitalWrite(D2, LOW);
